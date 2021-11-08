0625613bf804e3ffa706ba847c40485d70bfb57c
jackiepareja <pareja.jackie@gmail.com>
2021-11-08 08:47:59 -0800

    fixme: out of boundaries & scent logic

--- challenge.js
+++ challenge.js
@@ -1,13 +1,5 @@
 'use strict';
-
-/**
-   * Remaining To Dos:
-   * 1. Add logic where remaining robos can't pass through 'dead grids'.
-   * 2. Display robo command changes on browser. (canvas api?)
-   * 3. Fix parseInput - returned parsed object.robos isn't being accepted by the tickRobos() method.
-   * 4. Error on line 236: robo is being read as undefined. Fixed
-   */
-
+// constructor
 class Rover {
   constructor(x, y, o, command) {
     this.x = x;
@@ -17,21 +9,31 @@ class Rover {
   }
 }
 
+/**
+ * Helper Function
+ * @param {string, number, number} robot - rover's object's orientation, x-axis, y-axis
+ * @returns {number, number} newPosition - updated x-axis, y-axis
+ */
 function moveForward(robot) {
   const { o, x, y } = robot;
 
   const newPosition = { x, y };
 
   switch (o.toUpperCase()) {
-    case 'N': newPosition.y += 1; break;
+    case 'N': newPosition.y -= 1; break;
     case 'E': newPosition.x += 1; break;
-    case 'S': newPosition.y -= 1; break;
+    case 'S': newPosition.y += 1; break;
     case 'W': newPosition.x -= 1; break;
   }
-
   return newPosition;
 }
 
+/**
+ * Helper Function
+ * @param robot 
+ * @param {string} direction - R or L (right or left)
+ * @returns direction - updated direction
+ */
 function rotate(robot, direction) {
   switch (robot.o.toUpperCase()) {
     case 'N':
@@ -47,12 +49,15 @@ function rotate(robot, direction) {
 
 /* globals _, engine */
 window.initGame = function () {
-  //console.log('initgame');
   const limits = [];
   const command =
     '5 3 \n 1 1 s\n ffffff\n 2 1 w \n flfffffrrfffffff\n 0 3 w\n LLFFFLFLFL';
   let parsed;
 
+  /**
+   * @param input - accepts string commands
+   * @returns parsed - parsed input into an object
+   */
   const parseInput = (input) => {
     const commandsArray = input.split('\n ');
     const bounds = commandsArray[0].trim().split(' ');
@@ -70,43 +75,20 @@ window.initGame = function () {
     }
 
     parsed = { robos, bounds };
-    console.log('checking parsed obj', parsed)
 
     return parsed;
   };
 
 
-
+  /**
+   * 
+   * @param {array} robos - robos array object
+   * @returns robos - updating robos array objects
+   */
   const tickRobos = (robos) => {
-    console.log('robos', robos)
-    //console.log('tickrobos');
-    // 
-    // task #2
-    //
-    // in this function, write business logic to move robots around the playfield
-    // the 'robos' input is an array of objects; each object has 4 parameters.
-    // This function needs to edit each robot in the array so that its x/y coordinates
-    // and orientation parameters match the robot state after 1 command has been completed. 
-    // Also, you need to remove the command the robot just completed from the command list.
-    // example input:
-    //
-    // robos[0] = {x: 2, y: 2, o: 'N', command: 'frlrlrl'}
-    //
-    //                   - becomes -
-    // 
-    // robos[0] = {x: 2, y: 1, o: 'N', command: 'rlrlrl'} 
-    //
-    // if a robot leaves the bounds of the playfield, it should be removed from the robos
-    // array. It should leave a 'scent' in it's place. If another robot–for the duration
-    // of its commandset–encounters this 'scent', it should refuse any commands that would
-    // cause it to leave the playfield.
-
-    //console.log({ parsed })
 
     for (let i = 0; i < robos.length; i++) {
       const robot = robos[i];
-
-      console.log('Initial state', robot);
 
       if (!robot.command) {
         continue;
@@ -124,6 +106,7 @@ window.initGame = function () {
         })
 
         const prevPos = { x: robot.x, y: robot.y };
+
 
         // Check if out of bounds
         const [limitX, limitY] = parsed.bounds;
@@ -154,27 +137,21 @@ window.initGame = function () {
       }
 
       robot.command = robot.command.slice(1);
-
-      console.log(robot);
     }
-
     return robos;
+
   };
 
+  /**
+   * @param robos - most current robos object array 
+   */
   const missionSummary = (robos) => {
-    console.log('undefined robos', robos)
-    //
-    // task #3
-    //
-    // summarize the mission and inject the results into the DOM elements referenced in readme.md
-    //
 
     robos.forEach((robot, idx) => {
 
       const survived = !robot.died;
 
       const li = document.createElement('li');
-      console.log('robot.died', robot)
       if (survived) {
         li.innerHTML = `Robot ${idx} Position: ${robot.x}, ${robot.y} | Orientation: ${robot.o}`;
         document.querySelector('#robots').appendChild(li);
@@ -183,8 +160,6 @@ window.initGame = function () {
         document.querySelector('#lostRobots').appendChild(li);
       }
     });
-
-
     return;
   };
 
